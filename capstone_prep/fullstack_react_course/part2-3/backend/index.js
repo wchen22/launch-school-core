e1103ca336aa396c027d76a946b8c4d17e770d0d
Wilson Chen <wchen1222@gmail.com>
2025-08-09 17:05:02 -0400

    updated error handling middleware

--- capstone_prep/fullstack_react_course/part2-3/backend/index.js
+++ capstone_prep/fullstack_react_course/part2-3/backend/index.js
@@ -2,14 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const Note = require('./models/note')
 
-
 const app = express()
-
-const password = process.argv[2]
-const url = `mongodb+srv://fullstack:${password}@cluster0.bg5lweo.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
-
-
-console.log(password)
 
 let notes = [
   {
@@ -37,14 +30,9 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(requestLogger)
-app.use(express.static('dist'))
-
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -57,23 +45,38 @@ app.get('/api/notes', (request, response) => {
   // response.json(notes)
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  
-  if (note) {
-    response.json(note)
-  } else {
-    response.statusMessage = "That id don't exist"
-    response.status(404).end()
-  }
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+  .then(note => {
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+  })
+    .catch(error => next(error))
 })
+  // const id = request.params.id
+  // const note = notes.find(note => note.id === id)
+  
+  // if (note) {
+  //   response.json(note)
+  // } else {
+  //   response.statusMessage = "That id don't exist"
+  //   response.status(404).end()
+  // }
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
+   Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 
-  response.status(204).end()
+  // const id = request.params.id
+  // notes = notes.filter(note => note.id !== id)
+
+  // response.status(204).end()
 })
 
 const generateId = () => {
@@ -83,7 +86,7 @@ const generateId = () => {
   return String(maxId + 1)
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (!body.content) {
@@ -92,17 +95,65 @@ app.post('/api/notes', (request, response) => {
     })
   }
   
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-  response.json(note)
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))  
+  // {
+  //   content: body.content,
+  //   important: body.important || false,
+  //   id: generateId(),
+  // }
+
+  // notes = notes.concat(note)
+  // response.json(note)
 })
 
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content
+      note.important = important
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
+})
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id'})
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
